84e71cbbd88ec55df15c17e23005ca690a616b41
SulaRanawake <43115640+SulaRanawake@users.noreply.github.com>
2018-09-26 20:54:40 +0530

    Add files via upload

---
+++ Typing Trainer/src/viewUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Rushan
+ */
+public class viewUtil {
+    
+    public static ImageIcon getScaledImage(int x,int y,URL imgURL) throws MalformedURLException, IOException{
+        Image img = ImageIO.read(imgURL);
+        img = img.getScaledInstance(x, y, Image.SCALE_DEFAULT);
+        ImageIcon icon =  new ImageIcon(img);
+        return icon;
+    }
+}
---
+++ TypingTrainerTest/src/TestCase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sulakshana Ranawake
+ */
+public class TestCase {
+    public static void main(String args[]){
+        TestObject obj1 = new TestObject(10,20,30);
+        int vol = obj1.getVolume();
+        System.out.println(vol);
+    }
+}
